12723d73cf9ee7a5c706da09bf9d1b29d0d2cdeb
Edwin kipyego <koriredwin95@gmail.com>
2025-05-20 23:52:22 +0300

    organizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+('Mr. Chocolate', 8, 'M', 'brown', 'calm', 1),
+('Rowdy', 4, 'M', 'black', 'aggressive', 1),
+('Tabitha', 5, 'F', 'blonde', 'friendly', 1),
+('Sergeant Brown', 10, 'M', 'brown', 'grumpy', 1),
+('Melissa', 3, 'F', 'white', 'shy', 1),
+('Grinch', 6, 'M', 'green', 'grumpy', 0),
+('Wendy', 7, 'F', 'grey', 'calm', 1),
+(NULL, 2, 'F', 'black', 'wild', 1); 
